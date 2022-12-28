13376c42d944af8d2b65b3b0bc15314d401851a9
afaqalii <afaqali4244@gmail.com>
2022-12-28 22:02:36 +0500

    first commit

---
+++ landingPage.js
@@ -0,0 +1,15 @@
+$(".service-one").click(() => {
+    $(".service-body-one").toggle("hide")
+})
+
+$(".service-two").click(() => {
+    $(".service-body-two").toggle("hide")
+})
+
+$(".service-three").click(() => {
+    $(".service-body-three").toggle("hide")
+})
+
+$(".service-four").click(() => {
+    $(".service-body-four").toggle("hide")
+})
