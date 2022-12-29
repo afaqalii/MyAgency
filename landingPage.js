9fece957f0766ed3880239d63b35822f64a112fe
afaqalii <afaqali4244@gmail.com>
2022-12-29 15:34:35 +0500

    first commit

--- landingPage.js
+++ landingPage.js
@@ -1,3 +1,26 @@
+const menuItems = document.querySelector(".menu__items")
+const menuBars = document.querySelector(".menuBar")
+const menuBar = document.querySelectorAll(".bar")
+const menuItem = document.querySelectorAll(".menu_item")
+
+menuBars.addEventListener("click", () => {
+menuItems.classList.toggle("menu__itemsActive")
+menuBars.classList.toggle("menuBarActive")
+menuBar.forEach(bar => {
+    bar.classList.toggle("activated")
+})
+});
+menuItem.forEach(item => {
+    item.addEventListener("click", () => {
+        menuItems.classList.remove("menu__itemsActive")
+        menuBars.classList.remove("menuBarActive")
+         menuBar.forEach(bar => {
+            bar.classList.remove("activated")
+        })  
+    })
+})
+// menuBarActive
+
 $(".service-one").click(() => {
     $(".service-body-one").toggle("hide")
 })
